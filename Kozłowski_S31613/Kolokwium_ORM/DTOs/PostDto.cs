d0fe5f7c05e2cd3722b6a9bdf7a6940388e10f58
WKozlowski01 <s31613@pjwstk.edu.pl>
2025-06-10 17:13:30 +0200

    commit

---
+++ Kozłowski_S31613/Kolokwium_ORM/DTOs/PostDto.cs
@@ -0,0 +1,19 @@
+﻿using Kolokwium_ORM.Models;
+
+namespace Kolokwium_ORM.DTOs;
+
+public class PostDto
+{
+    public string raceName { get; set; }
+    public string trackName { get; set; }
+    public InesrtRaceParticipationDTO participation { get; set; }
+
+
+
+    public class InesrtRaceParticipationDTO
+    {
+        public int raceId { get; set; }
+        public int position { get; set; }
+        public int finishTimeInSeconds { get; set; }
+    }
+}
